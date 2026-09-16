2fbc4c12963ed0c8500f856115783f28fe332ea3
To Cau Sanchez <pk@tcsn.io>
2026-09-16 21:18:43 +0200

    refactor(forms): share native validity conversion

---
+++ packages/components/src/runtime/validity.ts
@@ -0,0 +1,14 @@
+export function validityStateToFlags(validity: ValidityState): ValidityStateFlags {
+  return {
+    badInput: validity.badInput,
+    customError: validity.customError,
+    patternMismatch: validity.patternMismatch,
+    rangeOverflow: validity.rangeOverflow,
+    rangeUnderflow: validity.rangeUnderflow,
+    stepMismatch: validity.stepMismatch,
+    tooLong: validity.tooLong,
+    tooShort: validity.tooShort,
+    typeMismatch: validity.typeMismatch,
+    valueMissing: validity.valueMissing,
+  };
+}
