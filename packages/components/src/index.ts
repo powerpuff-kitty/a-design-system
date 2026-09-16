8be3ce9e5ad12ecb2b65adc122ad3611cd84fb59
To Cau Sanchez <pk@tcsn.io>
2026-09-16 21:39:36 +0200

    feat(forms): export ads-switch

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -3,6 +3,7 @@ export type { AdsButtonType, AdsButtonVariant } from './button.js';
 export { AdsCheckbox, adsCheckboxContract } from './checkbox.js';
 export { AdsInput, adsInputContract } from './input.js';
 export type { AdsInputType, AdsSelectionDirection } from './input.js';
+export { AdsSwitch, adsSwitchContract } from './switch.js';
 export { AdsTextarea, adsTextareaContract } from './textarea.js';
 export type { AdsTextareaWrap } from './textarea.js';
 export * from './runtime/index.js';
